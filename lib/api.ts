69219e9365b782a935b0736c681003435cdb8623
Ruslan Bulimov <imrusbully@gmail.com>
2026-05-14 14:05:18 +0300

    Dockerfile

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080'
 
 interface AuthResponse {
   accessToken: string
